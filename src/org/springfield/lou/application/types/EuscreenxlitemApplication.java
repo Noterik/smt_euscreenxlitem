f34f7f8284d22778fc4f50c2907428a86e6ff4e3
Petar Stoyanov <petar@noterik.nl>
2016-04-19 11:01:27 +0300

    added copyright

--- src/org/springfield/lou/application/types/EuscreenxlitemApplication.java
+++ src/org/springfield/lou/application/types/EuscreenxlitemApplication.java
@@ -20,11 +20,16 @@
 */
 package org.springfield.lou.application.types;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
